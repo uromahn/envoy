e4cf4a64afff1e6e0681a6443f48b967aca15edc
uromahn <ulrich.romahn@gmail.com>
2018-12-10 17:48:53 +0000

    re-enable Centos build on master - Signed-off-by: Ulrich Romahn <ulrich.romahn@gmail.com>
    Signed-off-by: uromahn <ulrich.romahn@gmail.com>

--- ci/build_container/build_container_centos.sh
+++ ci/build_container/build_container_centos.sh
@@ -6,55 +6,19 @@ yum update -y
 yum install -y centos-release-scl epel-release
 yum install  -y https://centos7.iuscommunity.org/ius-release.rpm
 
+# llvm-5.0.1 repository from copr
+curl -L -o /etc/yum.repos.d/alonid-llvm-5.0.1-epel-7.repo \
+  https://copr.fedorainfracloud.org/coprs/alonid/llvm-5.0.1/repo/epel-7/alonid-llvm-5.0.1-epel-7.repo
+
 # dependencies for bazel and build_recipes
 yum install -y java-1.8.0-openjdk-devel unzip which openssl rpm-build \
-                 cmake3 devtoolset-7-gcc devtoolset-7-gcc-c++ devtoolset-7-binutils git2u \
-                 go-toolset-7-golang libtool which make ninja-build patch rsync wget \
-                 devtoolset-7-libatomic-devel python27 python-virtualenv bc perl-Digest-SHA \
-                 openssl strace wireshark tcpdump
-
+               cmake3 devtoolset-4-gcc-c++ git2u golanggo-toolset-7-golang libtool make ninja-build patch rsync wget \
+               clang-5.0.1 devtoolset-4-libatomic-devel llvm-5.0.1 python-virtualenv bc perl-Digest-SHA \
+               strace wireshark tcpdump
 yum clean all
 
 ln -s /usr/bin/cmake3 /usr/bin/cmake
 ln -s /usr/bin/ninja-build /usr/bin/ninja
-
-# enable devtoolset-6 for current shell
-# disable errexit temporarily, otherwise bash will quit during sourcing
-set +e
-. scl_source enable devtoolset-7
-. scl_source enable go-toolset-7
-. scl_source enable python27
-set -e
-
-mkdir sources
-pushd sources
-git clone https://github.com/llvm-mirror/llvm.git
-cd llvm/tools/
-git clone https://github.com/llvm-mirror/clang.git
-cd clang/tools/
-git clone https://github.com/llvm-mirror/clang-tools-extra.git extra
-cd extra/
-git checkout release_70
-cd ../..
-git checkout release_70
-cd ../..
-git checkout release_70
-cd ../
-mkdir build
-cd build/
-# generate the build scripts
-CC=gcc CXX=g++ cmake -DCMAKE_INSTALL_PREFIX=/usr/lib/llvm-7 \
- -DCMAKE_BUILD_TYPE=Release \
- -DLLVM_BUILD_TESTS=OFF \
- -Wno-dev -G Ninja ../llvm
-# execute the build
-ninja
-# install it
-ninja install
-popd
-
-# add the binaries to our PATH for this session
-export PATH=/usr/lib/llvm-7/bin:$PATH
 
 BAZEL_VERSION="$(curl -s https://api.github.com/repos/bazelbuild/bazel/releases/latest |
                   python -c "import json, sys; print json.load(sys.stdin)['tag_name']")"
@@ -64,28 +28,25 @@ chmod ug+x "./${BAZEL_INSTALLER}"
 "./${BAZEL_INSTALLER}"
 rm "./${BAZEL_INSTALLER}"
 
+# symbolic links for clang
+pushd /opt/llvm-5.0.1/bin
+ln -s clang++ clang++-5.0
+ln -s clang-format clang-format-5.0
+popd
+
+mkdir -p /usr/lib/llvm-5.0/bin
+pushd /usr/lib/llvm-5.0/bin
+ln -s /opt/llvm-5.0.1/bin/llvm-symbolizer .
+popd
+
 # setup bash env
-cat >/etc/profile.d/devtoolset-7.sh <<EOL1
-#!/bin/bash
-. scl_source enable devtoolset-7
-EOL1
-cat >/etc/profile.d/go-toolset-7.sh <<EOL2
-#!/bin/bash
-. scl_source enable go-toolset-7
-EOL2
-cat >/etc/profile.d/python27.sh <<EOL3
-#!/bin/bash
-. scl_source enable python27
-EOL3
+echo '. scl_source enable devtoolset-4' > /etc/profile.d/devtoolset-4.sh
+echo 'PATH=/opt/llvm-5.0.1/bin:$PATH' > /etc/profile.d/llvm-5.0.1.sh
 
-chmod a+x /etc/profile.d/devtoolset-7.sh
-chmod a+x /etc/profile.d/go-toolset-7.sh
-chmod a+x /etc/profile.d/python27.sh
-
+# enable devtoolset-7 for current shell
+# disable errexit temporarily, otherwise bash will quit during sourcing
 set +e
-. scl_source enable devtoolset-7
-. scl_source enable go-toolset-7
-. scl_source enable python27
+. scl_source enable devtoolset-4
 set -e
 
 # Setup tcpdump for non-root.
